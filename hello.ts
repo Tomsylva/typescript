aaf7d4fc63f5d31264d47aff2bb9f31397f8b843
Tomsylva <tomsylvanz@gmail.com>
2021-07-19 08:25:10 +0200

    initial commit

---
+++ hello.ts
@@ -0,0 +1,15 @@
+// function log(message){
+//     console.log(message)
+// }
+
+// let message = "Hello there"
+
+// log(message)
+
+function doSomething() {
+    for(let i = 0; i < 5; i++){
+        console.log(i)
+    }
+}
+
+doSomething();
