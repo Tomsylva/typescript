daddda80e81c513aa66074c381326f016f67c371
Tomsylva <tomsylvanz@gmail.com>
2021-07-29 10:44:57 +0200

    kata 2

--- katas.ts
+++ katas.ts
@@ -1,1 +1,3 @@
 // export const squareSum = (numbers: number[]): number => (numbers.map(x => x*x)).reduce(function(a, b){return a + b;}, 0);
+
+// export const getRealFloor = (n: number): number => n > 0 ? (n < 13 ? n - 1 : n - 2): n;
